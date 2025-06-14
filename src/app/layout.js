084e01f999a371cb3b79c1e664da9f338280625f
Aritol <maxdurant2001@gmail.com>
2025-06-14 12:20:02 +0300

    add google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -52,7 +52,30 @@ const RootLayout = async ({ children }) => {
     data?.layout || {}
   return (
     <html lang="uk">
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-XXXXXXX');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body className={clsx(rubik.variable, montserrat.variable)}>
+         {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header
           logo={logo.url}
           menu={menu}
